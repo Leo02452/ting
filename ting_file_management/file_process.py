2bcba69246ed1e70b100c7c9a8fa28a064173ce2
Leo Araujo <leonardo_02452@hotmail.com>
2022-12-03 19:16:37 -0300

    feat: implements process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in str(instance._data):
+        return None
+    file_lines = txt_importer(path_file)
+    file_dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file_lines),
+        'linhas_do_arquivo': file_lines
+    }
+    instance.enqueue(file_dict)
+    print(file_dict)
 
 
 def remove(instance):

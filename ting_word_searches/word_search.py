daf4b0efd5b330fbc1390261ab35c8172948006b
Leo Araujo <leonardo_02452@hotmail.com>
2022-12-06 19:56:53 -0300

    improve: separate get_word_ocurrencies feat into a new function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,19 +1,24 @@
-def exists_word(word, instance, get_content = False):
+def get_word_ocurrencies(word, file, get_content):
     search_result_list = []
+    occurrency_list = []
+    for index, line in enumerate(file['linhas_do_arquivo'], start=1):
+        if word.lower() in line.lower():
+            if get_content:
+                occurrency_list.append({'linha': index, 'conteudo': line})
+            else:
+                occurrency_list.append({'linha': index})
+    if len(occurrency_list):
+        search_result_list.append({
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': occurrency_list
+        })
+    return search_result_list
+
+
+def exists_word(word, instance, get_content=False):
     for file in instance._data:
-        occurrency_list = []
-        for index, line in enumerate(file['linhas_do_arquivo'], start=1):
-            if word.lower() in line.lower():
-                if get_content:
-                    occurrency_list.append({ 'linha': index, 'conteudo': line })
-                else:
-                    occurrency_list.append({ 'linha': index })
-        if len(occurrency_list):
-            search_result_list.append({
-                'palavra': word,
-                'arquivo': file['nome_do_arquivo'],
-                'ocorrencias': occurrency_list
-            })
+        search_result_list = get_word_ocurrencies(word, file, get_content)
     return search_result_list
 
 

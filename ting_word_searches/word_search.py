662915d425939d92f947807324f5c03c8302102b
Leo Araujo <leonardo_02452@hotmail.com>
2022-12-06 19:40:04 -0300

    feat: implements exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result_list = []
+    for file in instance._data:
+        occurrency_list = []
+        for index, line in enumerate(file['linhas_do_arquivo'], start=1):
+            if word.lower() in line.lower():
+                occurrency_list.append({ 'linha': index })
+        if len(occurrency_list):
+            search_result_list.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrency_list
+            })
+    return search_result_list
 
 
 def search_by_word(word, instance):
